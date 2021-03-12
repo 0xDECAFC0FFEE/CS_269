b025a408625f93ed963e689542493a9e2cded987
Shelly <shellyondiet@gmail.com>
2021-03-12 12:18:31 +0800

    with mixup

---
+++ MiniImagenet.py
@@ -0,0 +1,361 @@
+import os
+import torch
+from torch.utils.data import Dataset
+from torchvision.transforms import transforms
+import numpy as np
+import collections
+from PIL import Image
+import csv
+import random
+import torchvision
+from torchvision.transforms.functional import resize
+from .utils import fs_greedy_load
+import pickle
+from pathlib import Path
+from tqdm import tqdm
+import re
+import shutil
+import torch.nn.functional as F
+from sklearn.preprocessing import  OneHotEncoder
+
+
+
+class cached_transformer:
+    def __init__(self, images_source_path, image_filenames, cache, transform, reload_cache=False):
+        if reload_cache:
+            shutil.rmtree(cache)
+        try:
+            with open(cache/"image_path_to_id.pkl", "rb") as handle:
+                self.map = pickle.load(handle)
+
+            self.data = fs_greedy_load(cache/"data")
+        except FileNotFoundError:
+            print("transforming images and loading into memmapped cache")
+            os.makedirs(cache, exist_ok=True)
+
+            img_path_to_id = {path: id for id, path in enumerate(image_filenames)}
+
+            tramsformed_images = np.array([transform(images_source_path/path).numpy() for path in tqdm(image_filenames)])
+
+            self.data = fs_greedy_load(cache/"data", lst_array=tramsformed_images)
+            self.map = img_path_to_id
+            with open(cache/"image_path_to_id.pkl", "wb+") as handle:
+                pickle.dump(self.map, handle)
+            print("loaded transformed images into cache")
+
+    def __call__(self, path):
+        return self.data[self.map[path]]
+    
+    def __len__(self):
+        return len(self.data)
+
+class MiniImagenet(Dataset):
+    """
+    put mini-imagenet files as :
+    root :
+        |- images/*.jpg includes all imgeas
+        |- train.csv
+        |- test.csv
+        |- val.csv
+    NOTICE: meta-learning is different from general supervised learning, especially the concept of batch and set.
+    batch: contains several sets
+    sets: conains n_way * k_shot for meta-train set, n_way * n_query for meta-test set.
+    """
+
+    def __init__(self, root, mode, args, startidx=0):
+        """
+
+        :param root: root path of mini-imagenet
+        :param mode: train, val or test
+        :param batchsz: batch size of sets, not batch of imgs
+        :param n_way:
+        :param k_shot:
+        :param k_query: num of qeruy imgs per class
+        :param resize: resize to
+        :param startidx: start to index label from startidx
+        """
+
+        self.n_way = args["n_way"]  # n-way
+        self.k_shot = args["k_spt"]  # k-shot
+        self.k_query = args["k_qry"]  # for evaluation
+        self.setsz = self.n_way * self.k_shot  # num of samples per set
+        self.querysz = self.n_way * self.k_query  # number of samples per set for evaluation
+        self.resize = args["imgsz"]  # resize to
+        self.startidx = startidx  # index label not from 0, but from startidx
+        self.mode = mode 
+        # print('shuffle DB :%s, b:%d, %d-way, %d-shot, %d-query, resize:%d' % (
+        # mode, self.n_way, self.k_shot, self.k_query, self.resize))
+
+        csvdata = self.loadCSV(os.path.join(root, mode + '.csv'))  # csv path
+        self.data = []
+        self.img2label = {}
+        for i, (k, v) in enumerate(csvdata.items()):
+            self.data.append(v)  # [[img1, img2, ...], [img111, ...]]
+            self.img2label[k] = i + self.startidx  # {"img_name[:9]":label}
+        # print(csvdata)
+        from itertools import chain
+        print("self data", len(set(chain(*self.data))))
+        self.cls_num = len(self.data)
+
+        self.path = Path(root)/"images"  # image path
+        if mode == 'train':
+            self.batchsz = args.get("train_bs", 10000)  # batch of set, not batch of imgs
+            # transform = transforms.Compose([
+            #     lambda x: Image.open(x).convert('RGB'),
+            #     transforms.ToTensor(),
+            #     transforms.Resize((self.resize, self.resize)),
+            #     transforms.RandomResizedCrop(self.resize),
+            #     transforms.ColorJitter(.4, .4, .4, 0),
+            #     transforms.RandomHorizontalFlip(),
+            #     transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+            # ])
+            # self.transform = transform
+
+            tensor_transforms = torch.nn.Sequential(
+                transforms.Resize((self.resize, self.resize)),
+                transforms.RandomResizedCrop(self.resize),
+                transforms.ColorJitter(.4, .4, .4, 0),
+                transforms.RandomHorizontalFlip(),
+                # utils.mixup()
+                #transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+            )
+            self.transform = torch.jit.script(tensor_transforms)
+
+            # self.transform = cached_transformer(self.path, set(chain(*self.data)), Path(root)/"cache"/mode/"no_augment", transform)
+        else:
+            self.batchsz = args.get("test_bs", 100)  # batch of set, not batch of imgs
+            # transform = transforms.Compose([
+            #     lambda x: Image.open(x).convert('RGB'),
+            #     transforms.Resize((self.resize, self.resize)),
+            #     #  transforms.RandomResizedCrop(self.resize),
+            #     #  transforms.ColorJitter(.4, .4, .4, .4),
+            #     #  transforms.RandomHorizontalFlip(),
+            #      transforms.ToTensor(),
+            #     transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+            # ])
+            # self.transform = transform
+
+            tensor_transforms = torch.nn.Sequential(
+                transforms.Resize((self.resize, self.resize)),
+                transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+            )
+            self.transform = torch.jit.script(tensor_transforms)
+
+            # self.transform = cached_transformer(self.path, set(chain(*self.data)), Path(root)/"cache"/mode, transform)
+
+        self.create_batch(self.batchsz)
+
+        self.counts = {}
+
+    def loadCSV(self, csvf):
+        """
+        return a dict saving the information of csv
+        :param splitFile: csv file name
+        :return: {label:[file1, file2 ...]}
+        """
+        dictLabels = {}
+        with open(csvf) as csvfile:
+            csvreader = csv.reader(csvfile, delimiter=',')
+            next(csvreader, None)  # skip (filename, label)
+            for i, row in enumerate(csvreader):
+                filename = row[0]
+                label = row[1]
+                # append filename to current label
+                if label in dictLabels.keys():
+                    dictLabels[label].append(filename)
+                else:
+                    dictLabels[label] = [filename]
+        return dictLabels
+
+    def create_batch(self, batchsz):
+        """
+        create batch for meta-learning.
+        ×episode× here means batch, and it means how many sets we want to retain.
+        :param episodes: batch size
+        :return:
+        """
+        self.support_x_batch = []  # support set batch
+        self.query_x_batch = []  # query set batch
+        for b in range(batchsz):  # for each batch
+            # 1.select n_way classes randomly
+            selected_cls = np.random.choice(self.cls_num, self.n_way, False)  # no duplicate
+            np.random.shuffle(selected_cls)
+            support_x = []
+            query_x = []
+            for cls in selected_cls:
+                # 2. select k_shot + k_query for each class
+                selected_imgs_idx = np.random.choice(len(self.data[cls]), self.k_shot + self.k_query, False)
+                np.random.shuffle(selected_imgs_idx)
+                indexDtrain = np.array(selected_imgs_idx[:self.k_shot])  # idx for Dtrain
+                indexDtest = np.array(selected_imgs_idx[self.k_shot:])  # idx for Dtest
+                support_x.append(
+                    np.array(self.data[cls])[indexDtrain].tolist())  # get all images filename for current Dtrain
+                query_x.append(np.array(self.data[cls])[indexDtest].tolist())
+
+            # shuffle the correponding relation between support set and query set
+            random.shuffle(support_x)
+            random.shuffle(query_x)
+
+            self.support_x_batch.append(support_x)  # append set to current sets
+            self.query_x_batch.append(query_x)  # append sets to current sets
+    #========================================================================
+    def mixup_data(self, x, y, alpha=1.0, use_cuda=True):
+        '''Returns mixed inputs, pairs of targets, and lambda'''
+        if alpha > 0:
+            lam = np.random.beta(alpha, alpha)
+        else:
+            lam = 1
+
+        batch_size = x.size()[0]
+        if use_cuda:
+            index = torch.randperm(batch_size).cuda()
+        else:
+            index = torch.randperm(batch_size)
+
+        mixed_x = lam * x + (1 - lam) * x[index, :]
+        
+        #y_a, y_b = y, y[index]
+        mixed_y = lam * y + (1 - lam) * y[index, :]     
+        return mixed_x, mixed_y, lam
+
+    def mixup_criterion(criterion, pred, y_a, y_b, lam):
+        return lam * criterion(pred, y_a) + (1 - lam) * criterion(pred, y_b)
+    #========================================================================
+
+
+    def __getitem__(self, index):
+        """
+        index means index of sets, 0<= index <= batchsz-1
+        :param index:
+        :return:
+        """
+        # support_x shape = [setsz, 3, resize, resize]
+        support_x = torch.FloatTensor(self.setsz, 3, self.resize, self.resize)
+        # support_y shape = [setsz]
+        # query_x shape = [querysz, 3, resize, resize]
+        query_x = torch.FloatTensor(self.querysz, 3, self.resize, self.resize)
+        # query_y shape = [querysz]
+
+        flatten_support_x = [os.path.join(self.path, item) for sublist in self.support_x_batch[index] for item in sublist]
+        support_y = np.array(
+            [self.img2label[item[:9]]  # filename:n0153282900000005.jpg, the first 9 characters treated as label
+             for sublist in self.support_x_batch[index] for item in sublist]).astype(np.int32)
+
+        flatten_query_x = [os.path.join(self.path, item) for sublist in self.query_x_batch[index] for item in sublist]
+        query_y = np.array([self.img2label[item[:9]]
+                            for sublist in self.query_x_batch[index] for item in sublist]).astype(np.int32)
+
+
+
+        # print('global:', support_y, query_y)
+        # support_y: [setsz]
+        # query_y: [querysz]
+        # unique: [n-way], sorted
+        unique = np.unique(support_y)
+        random.shuffle(unique)
+        # relative means the label ranges from 0 to n-way
+        support_y_relative = np.zeros(self.setsz)
+        query_y_relative = np.zeros(self.querysz)
+        
+
+        for idx, l in enumerate(unique):
+            support_y_relative[support_y == l] = idx
+            query_y_relative[query_y == l] = idx
+
+        # print('relative:', support_y_relative, query_y_relative)
+
+        for i, path in enumerate(flatten_support_x):
+            self.counts[path] = self.counts.get(path, 0)+1
+            # support_x[i] = torch.FloatTensor(self.transform(path))
+            support_x[i] = self.transform(torchvision.transforms.functional.to_tensor(Image.open(path).convert('RGB')))
+
+        for i, path in enumerate(flatten_query_x):
+            self.counts[path] = self.counts.get(path, 0)+1
+            # query_x[i] = torch.FloatTensor(self.transform(path))
+            query_x[i] = self.transform(torchvision.transforms.functional.to_tensor(Image.open(path).convert('RGB')))
+        #====================================================================================================================
+        #Turn np array to tensor for one hot encoding 
+        torch_qyh = torch.from_numpy(query_y_relative)
+        onehot_query_y_relative = F.one_hot(torch_qyh.long(), num_classes=5)
+
+
+        #print("onehot",onehot_query_y_relative)
+
+        """
+        print("q_y_r",query_y_relative)
+        print("torch_qyh",torch_qyh)
+        print("onehot",onehot_query_y_relative)
+        """       
+        if self.mode == "train":
+            #do mixup
+            mixed_x , mixed_y, c = self.mixup_data(query_x,onehot_query_y_relative,alpha=1.0)
+
+            #return output
+            query_x = mixed_x
+            query_y_relative = mixed_y
+      
+            #print("query_y_relative",query_y)
+
+            #print("query_x",query_x)
+            #print("C",c)
+        else:
+            query_y_relative = torch.FloatTensor(onehot_query_y_relative.numpy())
+            #query_y_relative = onehot_query_y_relative
+            query_x = query_x
+        #====================================================================================================================
+        
+        #print(self.mode)
+        #print("shape",query_y_relative.shape)
+        #print("q_y_r",query_y_relative)
+        """
+
+        query_y = onehot_query_y_relative
+        query_x = query_x
+        """
+
+        if random.randint(0, 100) == 50:
+            print("average image augmentation calls: ", sum(self.counts.values())/len(self.counts), "total # of images:", len(self.counts))
+
+        #return support_x, torch.LongTensor(support_y_relative), query_x, torch.Tensor(query_y_relative)
+        return support_x, torch.FloatTensor(support_y_relative), query_x, query_y_relative
+
+
+
+    def __len__(self):
+        # as we have built up to batchsz of sets, you can sample some small batch size of sets.
+        return self.batchsz
+
+
+
+if __name__ == '__main__':
+    # the following episode is to view one set of images via tensorboard.
+    from torchvision.utils import make_grid
+    from matplotlib import pyplot as plt
+    from tensorboardX import SummaryWriter
+    import time
+
+    plt.ion()
+
+    tb = SummaryWriter('runs', 'mini-imagenet')
+    mini = MiniImagenet('../mini-imagenet/', mode='train', n_way=5, k_shot=1, k_query=1, batchsz=1000, resize=168)
+
+    for i, set_ in enumerate(mini):
+        # support_x: [k_shot*n_way, 3, 84, 84]
+        support_x, support_y, query_x, query_y = set_
+
+        support_x = make_grid(support_x, nrow=2)
+        query_x = make_grid(query_x, nrow=2)
+
+        plt.figure(1)
+        plt.imshow(support_x.transpose(2, 0).numpy())
+        plt.pause(0.5)
+        plt.figure(2)
+        plt.imshow(query_x.transpose(2, 0).numpy())
+        plt.pause(0.5)
+
+        tb.add_image('support_x', support_x)
+        tb.add_image('query_x', query_x)
+
+        time.sleep(5)
+
+    tb.close()
---
+++ load_data.py
@@ -0,0 +1,127 @@
+import torchvision
+import torch
+from pathlib import Path
+from .MiniImagenet import MiniImagenet
+import gdown
+import os
+import shutil
+import pickle
+from tqdm import tqdm
+import numpy as np
+from .utils import fs_greedy_load
+
+def cifar10(args):
+    dataset_path = Path(args.get("dataset_location", 'data/CFAR10'))
+
+    transform=torchvision.transforms.Compose([torchvision.transforms.ToTensor(),torchvision.transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+    train = torchvision.datasets.CIFAR10(dataset_path, train=True, download=True, transform=transform)
+    test = torchvision.datasets.CIFAR10(dataset_path, train=False, download=True, transform=transform)
+
+    n_train = int(len(train)*.8)
+    n_val = len(train)-n_train
+    train, val = torch.utils.data.random_split(train, [n_train, n_val])
+
+    train = torch.utils.data.DataLoader(train, batch_size=args["batch_size"], shuffle=True, num_workers=0,drop_last=False, pin_memory=True)
+    val = torch.utils.data.DataLoader(val, batch_size=args["batch_size"], shuffle=True, num_workers=0,drop_last=False, pin_memory=True)
+    test = torch.utils.data.DataLoader(test, batch_size=args["batch_size"], shuffle=False, num_workers=0,drop_last=True, pin_memory=True)
+
+    return train, val, test
+
+def mnist(args):
+    dataset_path = Path(args.get("dataset_location", 'data/MNIST'))
+
+    transform=torchvision.transforms.Compose([torchvision.transforms.ToTensor(), torchvision.transforms.Normalize((0.1307,), (0.3081,))])
+    train = torchvision.datasets.MNIST(dataset_path, train=True, download=True,transform=transform)
+    test = torchvision.datasets.MNIST(dataset_path, train=False, download=True,transform=transform)
+
+    n_train = int(len(train)*.8)
+    n_val = len(train)-n_train
+    train, val = torch.utils.data.random_split(train, [n_train, n_val])
+    
+    train = torch.utils.data.DataLoader(train, batch_size=args["batch_size"], shuffle=True, num_workers=0,drop_last=False, pin_memory=True)
+    val = torch.utils.data.DataLoader(val, batch_size=args["batch_size"], shuffle=True, num_workers=0,drop_last=False, pin_memory=True)
+    test = torch.utils.data.DataLoader(test, batch_size=args["batch_size"], shuffle=False, num_workers=0,drop_last=True, pin_memory=True)
+
+    return train, val, test
+
+def download_raw(dataset_path, saved_image_zip_file=None):
+    """
+    downloads raw maml jpeg images and csv train-val-test split files
+    """
+    print("downloading mini imagenet dataset (2.9 Gb)... ")
+
+    os.makedirs(dataset_path, exist_ok=True)
+    if saved_image_zip_file == None:
+        url = "https://drive.google.com/u/0/uc?id=1HkgrkAwukzEZA0TpO7010PkAOREb2Nuk"
+        import gdown
+        with open(dataset_path/"raw.zip", "wb+") as handle:
+            gdown.download(url, handle, quiet=False)
+    else:
+        os.system(f"cp {saved_image_zip_file} {dataset_path/'raw.zip'}")
+    
+    if not (dataset_path/'raw.zip').exists():
+        raise Exception(f"{dataset_path/'raw.zip'} doesn't exist")
+
+    os.system(f"unzip {dataset_path/'raw.zip'} -d {dataset_path}")
+
+    for filename in ["train.csv", "val.csv", "test.csv"]:
+        os.system(f"wget https://raw.githubusercontent.com/twitter/meta-learning-lstm/master/data/miniImagenet/{filename} -O {dataset_path/filename}")
+    os.system(f"rm -rf {dataset_path/'meta-learning-lstm'}")
+    os.system(f"rm {dataset_path/'raw.zip'}")
+
+def build_meta_learning_tasks(dataset_path, args, disable_training=False):
+    if not disable_training:
+        num_workers = 0
+        train = MiniImagenet(dataset_path, mode='train', args=args)
+        '''
+        train = MiniImagenet(dataset_path, mode='train', n_way=args["n_way"], k_shot=args["k_spt"],
+                            k_query=args["k_qry"],
+                            batchsz=args.get("train_bs", 10000), resize=args["imgsz"])
+        '''
+        train = torch.utils.data.DataLoader(train, args["task_num"], shuffle=False, num_workers=num_workers, pin_memory=True)
+
+        '''
+
+        val = MiniImagenet(dataset_path, mode='val', n_way=args["n_way"], k_shot=args["k_spt"],
+                                k_query=args["k_qry"],
+                                batchsz=args.get("test_bs", 100), resize=args["imgsz"])
+        '''
+        val = MiniImagenet(dataset_path, mode='val', args=args)
+        val = torch.utils.data.DataLoader(val, 1, shuffle=False, num_workers=num_workers, pin_memory=True)
+
+    else:
+        train, val = None, None
+
+    '''
+    test = MiniImagenet(dataset_path, mode='test', n_way=args["n_way"], k_shot=args["k_spt"],
+                             k_query=args["k_qry"],
+                             batchsz=args.get("test_bs", 100), resize=args["imgsz"])
+    '''
+    test = MiniImagenet(dataset_path, mode='test', args=args)
+    test = torch.utils.data.DataLoader(test, 1, shuffle=False, num_workers=num_workers, pin_memory=True)
+
+    return train, val, test
+
+def mini_imagenet(args, redownload=False, disable_training=False):
+    dataset_path = Path(args.get("dataset_location", "data/miniimagenet/"))
+    cache = dataset_path/"cache"
+    if redownload:
+        print(f"deleting dataset from {dataset_path}")
+        shutil.rmtree(dataset_path)
+
+    if not dataset_path.exists():
+        download_raw(dataset_path)
+
+    return build_meta_learning_tasks(dataset_path, args, disable_training=disable_training)
+
+def dataset(args, **kwargs):
+    dataset_name = args["dataset_name"]
+    
+    if dataset_name == "cifar10":
+        return cifar10(args, **kwargs)
+    elif dataset_name == "mnist":
+        return mnist(args, **kwargs)
+    elif dataset_name == "mini_imagenet":
+        return mini_imagenet(args, **kwargs)
+    else:
+        raise NotImplementedError(f"dataset {dataset_name} not implemented")
